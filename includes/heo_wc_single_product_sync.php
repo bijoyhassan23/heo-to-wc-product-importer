9ec122777ee17c5a73e2795c9fd7b5b94683caa8
Saif Uddin Bijoy <Bijoy@43.224.111.168.xpress.ltd>
2025-12-15 16:13:48 +0600

    stock sync updated

--- includes/heo_wc_single_product_sync.php
+++ includes/heo_wc_single_product_sync.php
@@ -11,8 +11,10 @@ trait HEO_WC_Single_Product_Sync {
         if (!$product instanceof WC_Product ) return;
         $product_id = $product->get_id();
         $last_sync = (int) get_post_meta($product_id, '_last_update', true);
-        $last_sync = $last_sync ? (time() - $last_sync) : 0;
-        if($last_sync <= 1800) return; // 30 minutes
+        if($last_sync){
+            $last_sync = $last_sync ? (time() - $last_sync) : 0;
+            if($last_sync <= 1800) return; // 30 minutes
+        }
         $sku = $product->get_sku();
         $this->product_stock_and_price_update(['product_id' => $product_id, 'sku' => $sku, 'sync' => 'both']);
         $this->log('Single product sync for product ID '.$product_id);
